99026e92b0d51bc74016bf620d119baeccbed4b1
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-03-25 16:55:30 +0500

    fix: add change emit

--- src/shared/ui/MySelect.vue
+++ src/shared/ui/MySelect.vue
@@ -1,20 +1,39 @@
 <script setup>
-import {defineProps} from 'vue';
+import {defineProps, ref} from 'vue';
 
-defineProps({
-  value: String,
+const props = defineProps({
+  modelValue: String,
   defaultValue: String,
   options: Array,
 });
+const emit = defineEmits(['update:modelValue']);
+
+const selectItem = ref(props.modelValue ?? "" );
+const setSelectItem = () => {
+  const item = props.options.find((itm) => itm.name === selectItem.value);
+  emit('update:modelValue', item);
+}
 </script>
 
 <template>
-  <select :value="value">
+  <select
+    class="select"
+    :value="selectItem"
+    v-model="selectItem"
+    @change="setSelectItem"
+  >
     <option disabled value="">{{defaultValue}}</option>
     <option
       v-for="option in options"
-      :key="option.value"
-      :value="option.value"
+      :key="option.id"
+      :value="option.name"
     >{{option.name}}</option>
   </select>
 </template>
+
+<style scoped>
+.select {
+  width: 100%;
+  padding: 15px 8px;
+}
+</style>
