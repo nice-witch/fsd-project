804895d2fd1c6682f3fa40610f9077f2fa90ee56
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-03-25 11:36:45 +0500

    add: select component

---
+++ src/shared/ui/MySelect.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+  value: String,
+  defaultValue: String,
+  options: Array,
+});
+</script>
+
+<template>
+  <select :value="value">
+    <option disabled value="">{{defaultValue}}</option>
+    <option
+      v-for="option in options"
+      :key="option.value"
+      :value="option.value"
+    >{{option.name}}</option>
+  </select>
+</template>
