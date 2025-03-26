63a25cb02b62eed4673e79a2af33c86722e7bc3e
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-03-26 13:12:03 +0500

    add: result form data

--- src/pages/ResultPage/ResultPage.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Result Page</h1>
-</template>
---
+++ src/pages/ResultPage/ui/ResultPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+const savedData = JSON.parse(localStorage.getItem("formData"));
+</script>
+
+<template>
+  <h1>Result Page</h1>
+
+  <p>Город: {{savedData[0].name}}</p>
+  <p>Цех: {{savedData[1].name}}</p>
+  <p>Сотрудник: {{savedData[2].name}}</p>
+  <p>Бригада: {{savedData[3].name}}</p>
+  <p>Смена: {{savedData[4].name}}</p>
+</template>
