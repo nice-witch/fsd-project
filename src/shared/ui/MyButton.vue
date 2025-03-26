f4b0c033a7a56ca960e7c3f2914cc35c49fcbbc4
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-03-26 11:59:22 +0500

    fix: add disabled style

--- src/shared/ui/MyButton.vue
+++ src/shared/ui/MyButton.vue
@@ -2,10 +2,29 @@
 import {defineProps} from 'vue';
 
 defineProps({
-  title: String
+  title: String,
+  disabled: Boolean,
 })
 </script>
 
 <template>
-  <button>{{title}}</button>
+  <button
+    :class="['button', {'disabled' : disabled}]"
+    :disabled="disabled"
+  >{{title}}</button>
 </template>
+
+<style scoped>
+.button {
+  padding: 10px;
+  background-color: brown;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.disabled {
+  background-color: grey;
+  cursor: auto;
+}
+</style>
